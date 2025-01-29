cb990f30288e01854dab6bbbe6bf5f80ff3b8df3
Arifa <arifaazar786@gmail.com>
2025-01-29 07:45:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function offSwitch(){
+    document.getElementById("bulbImage").src= "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("offButton").style.backgroundColor = "rgb(184, 179, 179)";
+    document.getElementById("onButton").style.backgroundColor = "green"
+    document.getElementById("switchHeading").textContent = "Switch Off"
+
+}
+
+function onSwitch(){
+    document.getElementById("bulbImage").src= "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("offButton").style.backgroundColor = "rgb(184, 179, 179)";
+    document.getElementById("onButton").style.backgroundColor = "red"
+    document.getElementById("switchHeading").textContent = "Switch On"
+}
